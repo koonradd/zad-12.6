b836782eb122c7c279c18fdfefaa8d55147a280f
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-02-09 22:03:06 +0100

    init

---
+++ js/scripts.js
@@ -0,0 +1,33 @@
+
+//nic  z tego nie rozumiem i nawet nie umiem skopiować dobrze zadania, bo cos nie działa
+
+
+var url = 'https://restcountries.eu/rest/v1/name/';
+var countriesList = $('#countries');
+
+$('#search').click(searchCountries);
+
+function searchCountries() {
+  	var countryName = $('#country-name').val();
+    if(!countryName.length) countryName = 'Poland';
+    
+    $.ajax({
+  		url: url + countryName,
+  		method: 'GET',
+  		success: showCountriesList
+  	});
+}
+
+function showCountriesList(resp) {
+  countriesList.empty();
+    
+    resp.forEach(function(item) {
+        
+        	$('<li>').text(item.name).appendTo(countriesList);
+        
+        return item.name;
+
+      
+    });
+}
+
