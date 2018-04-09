87eb797d3e0612c25c271c1117a0e6ec4fc0c430
Konrad Kościelski <konrad.koscielski@interia.pl>
2018-04-09 12:57:34 +0200

    new

--- js/scripts.js
+++ js/scripts.js
@@ -1,33 +1,32 @@
-
-//nic  z tego nie rozumiem i nawet nie umiem skopiować dobrze zadania, bo cos nie działa
-
-
-var url = 'https://restcountries.eu/rest/v1/name/';
-var countriesList = $('#countries');
-
-$('#search').click(searchCountries);
-
-function searchCountries() {
-  	var countryName = $('#country-name').val();
-    if(!countryName.length) countryName = 'Poland';
-    
-    $.ajax({
-  		url: url + countryName,
-  		method: 'GET',
-  		success: showCountriesList
-  	});
-}
+var url = "https://restcountries.eu/rest/v2/name/";
+var countriesList = $("#countries");
 
 function showCountriesList(resp) {
   countriesList.empty();
-    
-    resp.forEach(function(item) {
-        
-        	$('<li>').text(item.name).appendTo(countriesList);
-        
-        return item.name;
-
-      
-    });
+  resp.forEach(function(item) {
+    $("<li>")
+      .append($("<h3>").text(item.name))
+      .append($("<p>").text("Capital city: " + item.capital))
+      .append($("<p>").text("Region: " + item.region))
+      .append($("<p>").text("Population: " + item.population))
+      .append($("<p>").text("Area: " + item.area))
+      .append($("<p>").text("Time-zones: " + item.timezones))
+      .append(
+        $("<div>").append("<img " + 'class="flag"' + 'src="' + item.flag + '">')
+      )
+      .appendTo(countriesList);
+  });
 }
 
+function searchCountries() {
+  var countryName = $("#country-name").val();
+  if (!countryName.length) countryName = "Poland";
+  $.ajax({
+    url: url + countryName,
+    method: "GET",
+    success: showCountriesList
+  });
+}
+
+$("#search").click(searchCountries);
+$("#country-name").keypress(searchCountries);
